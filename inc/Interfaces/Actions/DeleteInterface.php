26e87c32a3987d62b5d931cc7a06c4641f3a876e
COQUARD Cyrille <coquardcyr@gmail.com>
2024-05-09 01:56:18 +0200

    Added settings

---
+++ inc/Interfaces/Actions/DeleteInterface.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace LaunchpadOptions\Interfaces\Actions;
+
+interface DeleteInterface
+{
+    /**
+     * Deletes the option with the given name.
+     *
+     * @param string $name Name of the option to delete.
+     *
+     * @return void
+     */
+    public function delete( string $name );
+}
---
+++ inc/Settings.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace LaunchpadOptions;
+
+class Settings implements Interfaces\SettingsInterface
+{
+
+    /**
+     * WordPress Option facade.
+     *
+     * @var Options
+     */
+    protected $options;
+
+    /**
+     * Settings option key.
+     *
+     * @var string
+     */
+    protected $settings_key;
+
+    /**
+     * Settings values.
+     *
+     * @var array
+     */
+    protected $settings = [];
+
+    /**
+     * Instantiate settings.
+     *
+     * @param Options $options WordPress Option facade.
+     * @param string $settings_key Settings option key.
+     */
+    public function __construct(Options $options, string $settings_key = 'settings')
+    {
+        $this->options = $options;
+        $this->settings_key = $settings_key;
+        $this->settings = $this->options->get($settings_key, []);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function get(string $name, $default = null)
+    {
+        /**
+         * Pre-filter any setting before read
+         *
+         * @param mixed $default The default value.
+         */
+        $value = apply_filters( "pre_get_{$this->settings}_" . $name, null, $default ); // phpcs:ignore WordPress.NamingConventions.PrefixAllGlobals.NonPrefixedHooknameFound
+
+        if ( null !== $value ) {
+            return $value;
+        }
+
+        if( ! $this->has($name)) {
+            return $default;
+        }
+
+        /**
+         * Filter any setting after read
+         *
+         * @param mixed $default The default value.
+         */
+        return apply_filters( "get_{$this->settings}" . $name, $this->settings[$name], $default ); // phpcs:ignore WordPress.NamingConventions.PrefixAllGlobals.NonPrefixedHooknameFound
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function set(string $name, $value)
+    {
+        $this->settings[$name] = $value;
+
+        $this->persist();
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function delete(string $name)
+    {
+        unset($this->settings[$name]);
+
+        $this->persist();
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function has(string $name): bool
+    {
+        return key_exists($name, $this->settings);
+    }
+
+    /**
+     * Persist the settings into the database.
+     * @return void
+     */
+    protected function persist()
+    {
+        do_action("pre_persist_{$this->settings_key}", $this->settings);
+
+        $this->options->set($this->settings_key, $this->settings);
+
+        do_action("persist_{$this->settings_key}", $this->settings);
+    }
+}
